fa778d6fe1ca7ef4d15c3afa3bad9eda01c57081
Nusrat Jahan Tanha <129551863+Nusrat-Jahan-Tanha@users.noreply.github.com>
2023-07-30 14:48:15 +0600

    Add files via upload

---
+++ tsk1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Hello in my university";
+    cout<<"\n";
+    cout<<"Im a student in it faculty";
+    cout<<"\n";
+    cout<<"Welcome.";
+}
