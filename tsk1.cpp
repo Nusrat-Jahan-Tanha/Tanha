dd233b51a329c8718f5a18578cbed6205c0c72b3
Nusrat Jahan Tanha <129551863+Nusrat-Jahan-Tanha@users.noreply.github.com>
2023-10-01 14:49:26 +0600

    Update tsk1.cpp

--- tsk1.cpp
+++ tsk1.cpp
@@ -1,10 +1,40 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-    cout<<"Hello in my university";
-    cout<<"\n";
-    cout<<"Im a student in it faculty";
-    cout<<"\n";
-    cout<<"Welcome.";
-}
+class Vehicle:
+    def __init__(self, wheels, range):
+        self.wheels = wheels
+        self.range = range
+
+    def display_info(self):
+        print("Wheels:", self.wheels)
+        print("Range:", self.range)
+
+
+class Car(Vehicle):
+    def __init__(self, wheels, range, passengers):
+        super().__init__(wheels, range)
+        self.passengers = passengers
+
+    def display_info(self):
+        print("Car:")
+        super().display_info()
+        print("Passengers:", self.passengers)
+
+
+class Truck(Vehicle):
+    def __init__(self, wheels, range, load_limit):
+        super().__init__(wheels, range)
+        self.load_limit = load_limit
+
+    def display_info(self):
+        print("Truck:")
+        super().display_info()
+        print("Load Limit:", self.load_limit)
+
+
+if __name__ == "__main__":
+    c = Car(4, 500, 5)
+    t = Truck(12, 1200, 3000)
+
+    c.display_info()
+    print()
+    t.display_info()
+
