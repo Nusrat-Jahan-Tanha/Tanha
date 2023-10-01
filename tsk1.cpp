baf17510f3f6af5feea49c0d377d38f9e15a73cf
Nusrat Jahan Tanha <129551863+Nusrat-Jahan-Tanha@users.noreply.github.com>
2023-10-01 15:11:42 +0600

    Update tsk1.cpp

--- tsk1.cpp
+++ tsk1.cpp
@@ -1,32 +1,58 @@
 #include <iostream>
+using namespace std;
 
 class Vehicle {
-public:
-    Vehicle(int wheels, int range) : wheels(wheels), range(range) {}
-    void display() const {
-        std::cout << "Wheels: " << wheels << "\nRange: " << range << std::endl;
-    }
-
-private:
+protected:
     int wheels;
     int range;
+
+public:
+    Vehicle(int w, int r) : wheels(w), range(r) {}
+
+    void displayInfo() {
+        cout << "Wheels: " << wheels << endl;
+        cout << "Range: " << range << endl;
+    }
 };
 
 class Car : public Vehicle {
-public:
-    Car(int wheels, int range, int passengers) : Vehicle(wheels, range), passengers(passengers) {}
-    void display() const {
-        std::cout << "Car:\n";
-        Vehicle::display();
-        std::cout << "Passengers: " << passengers << std::endl;
-    }
-
 private:
     int passengers;
+
+public:
+    Car(int w, int r, int p) : Vehicle(w, r), passengers(p) {}
+
+    void displayInfo() {
+        cout << "Car:" << endl;
+        Vehicle::displayInfo();
+        cout << "Passengers: " << passengers << endl;
+    }
 };
 
 class Truck : public Vehicle {
+private:
+    int loadLimit;
+
 public:
-    Truck(int wheels, int range, int loadLimit) : Vehicle(wheels, range), loadLimit(loadLimit) {}
+    Truck(int w, int r, int ll) : Vehicle(w, r), loadLimit(ll) {}
+
+    void displayInfo() {
+        cout << "Truck:" << endl;
+        Vehicle::displayInfo();
+        cout << "Load Limit: " << loadLimit << endl;
+    }
+};
+
+int main() {
+    Car c(4, 500, 5);
+    Truck t(12, 1200, 3000);
+
+    c.displayInfo();
+    cout << endl;
+    t.displayInfo();
+
+    return 0;
+}
+
 
 
